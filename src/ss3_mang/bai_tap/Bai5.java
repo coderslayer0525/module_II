ad2f59724620bd9e9de7d73667b6cac8ae7b7336
coderslayer0525 <legianghiem84@gmail.com>
2025-08-07 00:34:05 +0700

    bai_5

---
+++ src/ss3_mang/bai_tap/Bai5.java
@@ -0,0 +1,26 @@
+package ss3_mang.bai_tap;
+
+import java.util.Scanner;
+
+public class Bai5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số phần tử: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhập arr[" + i + "]: ");
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Giá trị nhỏ nhất trong mảng là: " + min);
+    }
+}
