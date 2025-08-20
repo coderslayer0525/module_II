26cd7e5514c7fecca058ac7603b238c16bfcdc13
coderslayer0525 <legianghiem84@gmail.com>
2025-08-20 14:14:39 +0700

    Triangle

--- src/ss13_debug_xulyngoaile/bai_tap/Triangle.java
+++ src/ss13_debug_xulyngoaile/bai_tap/Triangle.java
@@ -7,14 +7,17 @@ class Triangle {
 
     public Triangle(double a, double b, double c) throws IllegalTriangleException {
         if (a < 0 || b < 0 || c < 0) {
-            throw new IllegalTriangleException("phai lon hon 0");
+            throw new IllegalTriangleException("Phải lớn hơn 0");
         }
         if (a + b < c || a + c < b || b + c < a) {
-            throw new IllegalTriangleException(" tonng 2 canh phai lon hon canh con lai");
+            throw new IllegalTriangleException(" Tổng 2 cạnh phải lớn hơn cạnh còn lại");
+        }
+        if(a + b > c || a + c > b || b + c > a){
+            System.out.println(" đây là 1 tam giác");
         }
     }
 
     public void display() {
-        System.out.println("la 1 tam giac");
+        System.out.println("Chính xác là 1 tam giác");
     }
 }
